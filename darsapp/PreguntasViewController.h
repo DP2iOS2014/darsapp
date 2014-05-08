9fe870755976a0c987f8e4d9fe5d1a102c8d1c2d
jameschirinos <jamesj2809@gmail.com>
2014-05-07 22:00:44 -0500

    AvanzeJuego
    bomba + tiempo

--- darsapp/PreguntasViewController.h
+++ darsapp/PreguntasViewController.h
@@ -9,5 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface PreguntasViewController : UIViewController
+{
+    NSArray *ArregloPreguntas;
+}
+@property (weak, nonatomic) IBOutlet UILabel *VidasReloj;
+@property (weak, nonatomic) IBOutlet UIButton *btnReloj;
+@property (weak, nonatomic) IBOutlet UIButton *btnBomba;
+@property (weak, nonatomic) IBOutlet UIButton *btnOtroOpcion;
+@property (weak, nonatomic) IBOutlet UIButton *btnNuevaPregunta;
+@property (weak, nonatomic) IBOutlet UILabel *Tiempo;
+@property (weak, nonatomic) IBOutlet UIButton *btnRespuesta1;
+@property (weak, nonatomic) IBOutlet UIButton *btnRespuesta2;
+@property (weak, nonatomic) IBOutlet UIButton *btnRespuesta3;
+@property (weak, nonatomic) IBOutlet UIButton *btnRespuesta4;
+@property (weak, nonatomic) IBOutlet UILabel *VidasBomba;
+@property (weak, nonatomic) IBOutlet UILabel *VidasOtraOpcion;
+@property (weak, nonatomic) IBOutlet UILabel *pregunta;
 
 @end
--- darsapp/PreguntasViewController.m
+++ darsapp/PreguntasViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "PreguntasViewController.h"
+#import "ClasePregunta.h"
+#import "ClaseRespuesta.h"
 
 @interface PreguntasViewController ()
 
@@ -26,7 +28,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    ClaseRespuesta *resp1=[[ClaseRespuesta alloc] initConRespuesta:@"Naty" escorrecto:NO];
+    ClaseRespuesta *resp2=[[ClaseRespuesta alloc] initConRespuesta:@"Jorge" escorrecto:NO];
+    ClaseRespuesta *resp3=[[ClaseRespuesta alloc] initConRespuesta:@"Karina" escorrecto:NO];
+    ClaseRespuesta *resp4=[[ClaseRespuesta alloc] initConRespuesta:@"James" escorrecto:YES];
+    NSArray *ArregloResp=[NSArray arrayWithObjects:resp1,resp2,resp3,resp4,nil];
+    ClasePregunta *pregunta1=[[ClasePregunta alloc] initConPregunta:@"Como me Llamo"  Respuestas:ArregloResp];
+    ClaseRespuesta *resp5=[[ClaseRespuesta alloc] initConRespuesta:@"Naty" escorrecto:NO];
+    ClaseRespuesta *resp6=[[ClaseRespuesta alloc] initConRespuesta:@"Jorge" escorrecto:NO];
+    ClaseRespuesta *resp7=[[ClaseRespuesta alloc] initConRespuesta:@"Karina" escorrecto:NO];
+    ClaseRespuesta *resp8=[[ClaseRespuesta alloc] initConRespuesta:@"James" escorrecto:YES];
+    NSArray *ArregloResp1=[NSArray arrayWithObjects:resp5,resp6,resp7,resp8,nil];
+    ClasePregunta *pregunta2=[[ClasePregunta alloc] initConPregunta:@"Como me Llamo"  Respuestas:ArregloResp1];
+    
+    
+    
+    ArregloPreguntas =[NSArray arrayWithObjects:pregunta1,pregunta2,nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,6 +60,72 @@
 {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)SeApretoReloj:(id)sender {
+    if( [self.VidasReloj.text intValue]  >0) {
+        int value= [self.Tiempo.text intValue]+30;
+        self.Tiempo.text= [[NSString alloc] initWithFormat:@"%d",value] ;
+        self.VidasReloj.text=[[NSString alloc] initWithFormat:@"%d", [ self.VidasReloj.text intValue]-1];
+        self.btnReloj.enabled=NO;
+        self.btnBomba.enabled=NO;
+        self.btnOtroOpcion.enabled=NO;
+        self.btnNuevaPregunta.enabled=NO;
+    }
+}
+- (IBAction)SeApretoBomba:(id)sender {
+    
+    if( [self.VidasBomba.text intValue]  >0) {
+        //int value= [self.Tiempo.text intValue]+30;
+        //self.Tiempo.text= [[NSString alloc] initWithFormat:@"%d",value] ;
+        
+        int valor1 = arc4random()%4;
+        int valor2 = arc4random()%4;
+        while(valor1 == valor2){
+            valor2 = arc4random()%4;
+        }
+        
+        if (valor1 == 1 || valor2 == 1) {
+            self.btnRespuesta1.enabled=NO;
+        }
+        if (valor1 == 2 || valor2 == 2) {
+            self.btnRespuesta2.enabled=NO;
+        }
+        if (valor1 == 3 || valor2 == 3) {
+            self.btnRespuesta3.enabled=NO;
+        }
+        if (valor1 == 4 || valor2 == 4) {
+            self.btnRespuesta4.enabled=NO;
+        }
+        
+        self.VidasBomba.text=[[NSString alloc] initWithFormat:@"%d", [ self.VidasReloj.text intValue]-1];
+        self.btnReloj.enabled=NO;
+        self.btnBomba.enabled=NO;
+        self.btnOtroOpcion.enabled=NO;
+        self.btnNuevaPregunta.enabled=NO;
+    }
+    
+    
+    
+}
+
+- (IBAction)SeApretoOtraOpcion:(id)sender {
+    if( [self.VidasBomba.text intValue]  >0) {
+        self.VidasBomba.text=[[NSString alloc] initWithFormat:@"%d", [ self.VidasReloj.text intValue]-1];
+        self.btnReloj.enabled=NO;
+        self.btnBomba.enabled=NO;
+        self.btnOtroOpcion.enabled=NO;
+        self.btnNuevaPregunta.enabled=NO;
+    }
+}
+/*
+- (void)viewWillAppear:(BOOL)animated {
+    self.pregunta.text=(NSString*)[[ArregloPreguntas objectAtIndex:0] Pregunta];
+    NSArray *resp=[[ArregloPreguntas objectAtIndex:0] ArregloRespuestas];
+    self.btnRespuesta1.titleLabel.text= [resp objectAtIndex:0 ];
+    self.btnRespuesta2.titleLabel.text= [resp objectAtIndex:1 ];
+    self.btnRespuesta3.titleLabel.text= [resp objectAtIndex:2 ];
+    self.btnRespuesta4.titleLabel.text= [resp objectAtIndex:3 ];
 }
 */
 

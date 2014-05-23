3b4b21f1304688fb52d371eb0cd5492559b1ef44
karina0311 <inf227al@iMac-de-ladmin-4.local>
2014-05-23 12:19:18 -0500

    Estandarizacion

---
+++ darsapp/GrillaInicialAmbientalizate.m
@@ -0,0 +1,48 @@
+//
+//  GrillaInicialAmbientalizate.m
+//  darsapp
+//
+//  Created by inf227al on 23/05/14.
+//  Copyright (c) 2014 ___greensoft___. All rights reserved.
+//
+
+#import "GrillaInicialAmbientalizate.h"
+
+@interface GrillaInicialAmbientalizate ()
+
+@end
+
+@implementation GrillaInicialAmbientalizate
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.parentViewController.view.backgroundColor= [UIColor colorWithPatternImage:[UIImage imageNamed:@"congruent_pentagon.png"]];
+    self.tableView.backgroundColor = [UIColor clearColor];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end

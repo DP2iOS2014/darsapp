f7420bb993cbe8fba32df4891ca5c07dc288e61b
James Chirinos <jamesj2809@gmail.com>
2014-05-29 15:46:23 -0500

    temas con imaegnes

--- darsapp/TemasAdmTableViewController.m
+++ darsapp/TemasAdmTableViewController.m
@@ -21,6 +21,7 @@
     NSDictionary *respuestatemas;
     NSMutableArray *arregloRespuesta;
     NSMutableArray *titulos;
+    NSMutableArray *nombresimagenes;
     
 
 }
@@ -39,6 +40,7 @@
     [super viewDidLoad];
     //[self recuperaEcoTipsLider];
     titulos = [[NSMutableArray alloc] init];
+    nombresimagenes= [[NSMutableArray alloc] init];
     [self recuperoTemas];
     
     
@@ -82,12 +84,20 @@
 {
 
     // Return the number of rows in the section.
-    return titulos.count ;
+    return (titulos.count + 1) ;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    CeldaTemas *cell = [tableView dequeueReusableCellWithIdentifier: @"CeldaTemas"];
-    cell.lblTema.text=titulos[indexPath.row];
+    UITableViewCell * cell ;
+    if(indexPath.row < titulos.count){
+        cell = [tableView dequeueReusableCellWithIdentifier: @"CeldaTemas"];
+        ((CeldaTemas*)cell).lblTema.text=titulos[indexPath.row];
+        ((CeldaTemas*)cell).imagen.image=[UIImage imageNamed:nombresimagenes[indexPath.row]];
+
+    }else{
+        cell = [tableView dequeueReusableCellWithIdentifier: @"celdaguardar"];
+    }
+
     return cell;
 }
 
@@ -116,6 +126,7 @@
             NSString *nombreimagen = [[arregloPosiciones objectAtIndex:i] objectForKey:@"nombre_archivo"];
             
             [titulos addObject:titulo];
+            [nombresimagenes addObject:nombreimagen];
             
         }
         
@@ -131,78 +142,6 @@
           }];
 }
 
--(void) recuperaEcoTipsLider{
-
-    //PARA ENVIAR PRIMI
-    
-    NSDictionary *cuerpo = [NSDictionary dictionaryWithObjectsAndKeys:@"buenaspracticas", @"tipo", nil];
-    NSDictionary * consulta = [NSDictionary dictionaryWithObjectsAndKeys:@"Consulta",@"operacion",cuerpo,@"cuerpo" , nil];
-    
-    NSLog(@"%@", consulta);
-    
-    
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    manager.requestSerializer = [AFJSONRequestSerializer serializer];
-    
-    [manager POST:Buenaspracticas parameters:consulta success:^(AFHTTPRequestOperation *task, id responseObject) {
-        respuesta = responseObject;
-        NSLog(@"JSON: %@", respuesta);
-        
-        NSDictionary * diccionarioPosiciones = [respuesta objectForKey:@"cuerpo"];
-        NSArray * arregloPosiciones = [diccionarioPosiciones objectForKey:@"listaNodos"];
-        
-        //PARA SACAR LOS DATOS
-        
-        for(int i=0; i<[arregloPosiciones count];i++){
-        NSString *descripcion = [[arregloPosiciones objectAtIndex:i] objectForKey:@"descripcion"];
-        NSString *titulo = [[arregloPosiciones objectAtIndex:i] objectForKey:@"title"];
-        NSString *puntaje = [[arregloPosiciones objectAtIndex:i] objectForKey:@"puntaje"];
-        NSString *tema = [[arregloPosiciones objectAtIndex:i] objectForKey:@"tema"];
-        double punt=[puntaje doubleValue];
-        double idtema;
-            
-        if ([tema isEqualToString:@"agua"]) {
-            idtema=1;
-        };
-            if ([tema isEqualToString:@"espacios verdes"]) {
-                idtema=2;
-            };
-            if ([tema isEqualToString:@"compras"]) {
-                idtema=3;
-            };
-            if ([tema isEqualToString:@"energia"]) {
-                idtema=4;
-            };
-            if ([tema isEqualToString:@"liderazgo"]) {
-                idtema=5;
-            };
-            if ([tema isEqualToString:@"papeles"]) {
-                idtema=6;
-            };
-            if ([tema isEqualToString:@"residuos solidos"]) {
-                idtema=7;
-            };
-            if ([tema isEqualToString:@"aire y ruido"]) {
-                idtema=8;
-            };
-            if ([tema isEqualToString:@"tranporte"]) {
-                idtema=9;
-            };
-            
-            
-     
-        
-        }
-    }
-          failure:^(AFHTTPRequestOperation *task, NSError *error) {
-              UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"No choco con el servidor"
-                                                                  message:[error localizedDescription]
-                                                                 delegate:nil
-                                                        cancelButtonTitle:@"Ok"
-                                                        otherButtonTitles:nil];
-              [alertView show];
-          }];
-}
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     

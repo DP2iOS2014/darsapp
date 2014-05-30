748226ae46253142ce3f509d7a8602c3b59a84e8
James Chirinos <jamesj2809@gmail.com>
2014-05-30 13:06:34 -0500

    Flujo del Juego
    instrucciones y comodines

---
+++ Instrucciones3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Instrucciones3ViewController.h
+//  darsapp
+//
+//  Created by inf227al on 30/05/14.
+//  Copyright (c) 2014 ___greensoft___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Instrucciones3ViewController : UIViewController
+
+@end
